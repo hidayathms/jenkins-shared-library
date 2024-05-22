fd71a330b9b5592495a36df7c494a6477e600e3d
Hidayath <hidayath@aol.com>
2024-05-22 18:09:58 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,28 @@
 
-def lintchecks() {
-    sh "echo ***** Starting Style checks *******"
-    // sh "npm i jslint"
+def lintchecks('component') {
+    sh "echo ***** Starting Style checks for ${component} *******"
+    // sh "npm intstall jslint"
     sh "node_modules/jslint/bin/jslint.js server.js || true"
     sh "echo **** Starting Style checks *******"
+}
+
+def call() {
+pipeline { 
+    agent any
+    stages {
+        stage('Lint Checks') {
+            steps {
+                script {
+                    nodejs.lintchecks()
+                }
+            }
+        }
+        stage('Static Code Analysis') {
+            steps {
+                sh "echo Starting Static code analysis"
+            }
+        }
+    }
+}
+
 }
