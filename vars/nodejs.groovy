2a2988ffd2232e58729538e3461f0c094838ec92
Hidayath <hidayath@aol.com>
2024-05-22 18:14:56 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,7 +13,7 @@ pipeline {
         stage('Lint Checks') {
             steps {
                 script {
-                    nodejs.lintchecks()
+                    lintchecks()
                 }
             }
         }
