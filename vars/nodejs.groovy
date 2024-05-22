bc45f124acd189b95ed71d15523ab50f77a1ebf1
Hidayath <hidayath@aol.com>
2024-05-22 17:43:18 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,8 @@
 
 def lintchecks() {
     sh "echo ***** Starting Style checks *******"
-    sh "npm i jslint"
+    sh "npm install jslint"
     sh "ls -ltr node_modules/jslint/bin"
-    sh "/home/centos/node_modules/jslint/bin/jslint.js server.js || true"
+    sh "/node_modules/jslint/bin/jslint.js server.js || true"
     sh "echo **** Starting Style checks *******"
 }
