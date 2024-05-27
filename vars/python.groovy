2d2d4982c39e217748fbea0ff164c86e97856c15
Hidayath <hidayath@aol.com>
2024-05-27 21:07:04 +0530

    Jenkin Pipeline

--- vars/python.groovy
+++ vars/python.groovy
@@ -5,10 +5,6 @@ def lintchecks() {
     sh "echo **** Style checks are completed for ${COMPONENT} *******"
 }
 
-def sonarChecks() {
-    sh "sonar-scanner -Dsonar.host.url=http://54.224.63.79:9000 -Dsonar.java.binaries=./target -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=pass"
-}
-
 def call() {
 pipeline { 
     agent any
