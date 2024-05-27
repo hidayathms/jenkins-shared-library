e5ec220761700528abff84517bcdda1f853fa2d2
Hidayath <hidayath@aol.com>
2024-05-27 20:40:40 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,7 +7,7 @@ def lintchecks() {
 }
 
 def sonarChecks() {
-    sh "sonar-scanner -Dsonar.host.url=http://172.31.41.5:9000 -Dsonar.sources=. -Dsonar.projectKey=${component} -Dsonar.login=admin -Dsonar.password=pass"
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.41.5:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=pass"
     }
 
 def call() {
