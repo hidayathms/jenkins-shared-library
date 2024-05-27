87bc9a23d1eb0e08665e701d2c3b28884b1fd0cd
Hidayath <hidayath@aol.com>
2024-05-27 20:26:15 +0530

    Jenkin Pipeline

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,15 +1,15 @@
 
-def lintchecks(component) {
-    sh "echo ***** Starting Style checks for  ${component} *******"
+def lintchecks() {
+    sh "echo ***** Starting Style checks for  ${COMPONENT} *******"
     sh "mvn checkstyle:check || true"
-    sh "echo **** Style checks are completed for ${component} *******"
+    sh "echo **** Style checks are completed for ${COMPONENT} *******"
 }
 
-def sonarChecks(component) {
+def sonarChecks() {
     sh "sonar-scanner -Dsonar.host.url=http://54.224.63.79:9000 -Dsonar.java.binaries=./target -Dsonar.projectKey=${component} -Dsonar.login=admin -Dsonar.password=pass"
     }
 
-def call(component) {
+def call() {
 pipeline { 
     agent any
     tools {
@@ -19,7 +19,7 @@ pipeline {
         stage('Lint Checks') {
             steps {
                 script {
-                    lintchecks(component)
+                    lintchecks()
                 }
             }
         }
@@ -32,14 +32,14 @@ pipeline {
         stage('Static Code Analysis') {
             steps {
                 script {
-                    sonarChecks(component)
+                    sonarChecks()
                 }
             }
         }
         stage('Get the sonar results') {
             steps {
                 sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh"
-                sh "bash gates.sh admin pass 172.31.41.5 ${component}"
+                sh "bash gates.sh admin pass 172.31.41.5 ${COMPONENT}"
                 }
             }
     }
