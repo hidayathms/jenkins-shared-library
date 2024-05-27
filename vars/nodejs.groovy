f31fd2e1f800e7334229e218648c2dc1c562a34d
Hidayath <hidayath@aol.com>
2024-05-27 11:15:34 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,10 +7,8 @@ def lintchecks(component) {
 }
 
 def sonarChecks(){
-    sh ...
-    sonar-scanner -Dsonar.host.url=http://172.31.41.5:9000 -Dsonar.soruces=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=pass
-    ...
-}
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.41.5:9000 -Dsonar.soruces=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=pass"
+    }
 def call(component) {
 pipeline { 
     agent any
