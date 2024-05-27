a3324508b517dc788c9eeb8c305b7c707d339d99
Hidayath <hidayath@aol.com>
2024-05-27 15:12:49 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -31,8 +31,7 @@ def call(component) {
         stage('Get the sonar results') {
             steps {
                 sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh |sudo bash"
-                sh "echo "DevOps321" | sudo -S"
-                sh "./gates.sh admin pass 172.31.41.5 ${component}"
+                sh "echo "DevOps321" | sudo -S ./gates.sh admin pass 172.31.41.5 ${component}"
                 }
             }
         stage('Unit Testing') {
