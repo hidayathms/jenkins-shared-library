539d3a4cc7c1785abe06d707a8621bbce8097e2d
Hidayath <hidayath@aol.com>
2024-05-27 20:16:15 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,37 +1,37 @@
 
-def lintchecks(component) {
-    sh "echo ***** Starting Style checks for  ${component} *******"
+def lintchecks() {
+    sh "echo ***** Starting Style checks for  ${COMPONENT} *******"
     // sh "npm i jslint"
     sh "node_modules/jslint/bin/jslint.js server.js || true"
-    sh "echo **** Style checks are completed for ${component} *******"
+    sh "echo **** Style checks are completed for ${COMPONENT} *******"
 }
 
-def sonarChecks(component) {
+def sonarChecks() {
     sh "sonar-scanner -Dsonar.host.url=http://172.31.41.5:9000 -Dsonar.sources=. -Dsonar.projectKey=${component} -Dsonar.login=admin -Dsonar.password=pass"
     }
 
-def call(component) {
+def call() {
   pipeline { 
     agent any
     stages {
         stage('Lint Checks') {
             steps {
                 script {
-                    lintchecks(component)
+                    lintchecks()
                 }
             }
         }
         stage('Static Code Analysis') {
             steps {
                 script {
-                    sonarChecks(component)
+                    sonarChecks()
                 }
             }
         }
         stage('Get the sonar results') {
             steps {
                 sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh"
-                sh "bash gates.sh admin pass 172.31.41.5 ${component}"                }
+                sh "bash gates.sh admin pass 172.31.41.5 ${COMPONENT}"                }
             }
         stage('Unit Testing') {
             steps {
