85b23702d013489cafec1ff4215c46baa5528586
Hidayath <hidayath@aol.com>
2024-05-28 18:27:59 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -32,8 +32,8 @@ def call() {
         }
         stage('Get the sonar results') {
             steps {
-                sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh"
-                sh "bash gates.sh admin pass ${SONAR_URL} ${COMPONENT}"                }
+            //     sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh"
+            //     sh "bash gates.sh admin pass ${SONAR_URL} ${COMPONENT}"                }
             }
         stage ('Test Cases'){
         parallel {
