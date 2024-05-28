0f25eabd2faedda7966368a6b74084b0ffd32b14
Hidayath <hidayath@aol.com>
2024-05-28 09:50:18 +0530

    Jenkin Pipeline

--- vars/python.groovy
+++ vars/python.groovy
@@ -56,13 +56,13 @@ pipeline {
                        sh "echo Functional Testing completed"
                     }
                 }
-                stage('Prepare Artifact') {
+                stage('Prepare Artifact') {  // Runs only when you run this job from tag and from branches it should run
                     steps {
                        sh "echo Prparing Artifacts in progress"
                        sh "echo Artifact build  completed"
                     }
                 }
-                stage('Uploding the Artifacts') {
+                stage('Uploding the Artifacts') {       // Runs only when you run this job from tag and from branches it should run
                     steps {
                        sh "echo Uploading the Artifacts in progress"
                        sh "echo Artifacts Uload completed"
