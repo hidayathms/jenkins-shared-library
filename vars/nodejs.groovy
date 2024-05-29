126bf0d1057b54dd61e832123f74b4600ae66587
Hidayath <hidayath@aol.com>
2024-05-29 13:40:50 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -71,14 +71,14 @@ def call() {
                     when { expression {env.TAG_NAME != null} }
                     steps {
                         script {
-                          env.UPLOAD_STATUS = sh (returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true") 
+                          env.UPLOAD_STATUS = sh(returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true") 
                           print UPLOAD_STATUS                      
                         }
                     }
                 }
                 stage('Prepare Artifact') {     // Runs only when you run this job from tag and from branches it should run
                     when { 
-                        expression {env.TAG_NAME != null} 
+                        // expression {env.TAG_NAME != null} 
                         expression {env.UPLOAD_STATUS == null} 
                         }
                     steps {
@@ -91,7 +91,7 @@ def call() {
                 }
                 stage('Uploding the Artifacts') {       // Runs only when you run this job from tag and from branches it should run
                     when { 
-                        expression {env.TAG_NAME != null} 
+                        // expression {env.TAG_NAME != null} 
                         expression {env.UPLOAD_STATUS == null} 
                         }
                     steps {
