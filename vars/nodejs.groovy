39e572b6f61cee0a4ba8a609c5d45561b81bf6d7
Hidayath <hidayath@aol.com>
2024-05-29 13:43:14 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -78,7 +78,7 @@ def call() {
                 }
                 stage('Prepare Artifact') {     // Runs only when you run this job from tag and from branches it should run
                     when { 
-                        expression {env.TAG_NAME != null} &&
+                        expression {env.TAG_NAME != null}
                         expression {env.UPLOAD_STATUS == null} 
                         }
                     steps {
