0abcedcd69368a21ddb369bd673b0c677820fa24
Hidayath <hidayath@aol.com>
2024-05-29 13:27:07 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -92,7 +92,7 @@ def call() {
                 stage('Uploding the Artifacts') {       // Runs only when you run this job from tag and from branches it should run
                     when { 
                         expression {env.TAG_NAME != null} 
-                        // expression {env.UPLOAD_STATUS == ""} 
+                        expression {env.UPLOAD_STATUS == ""} 
                         }
                     steps {
                        sh "echo Uploading the Artifacts in progress"
