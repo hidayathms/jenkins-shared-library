7a8cc2c67deba3e87e06e709732766236a7f1ba9
Hidayath <hidayath@aol.com>
2024-05-29 11:17:10 +0530

    Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -77,7 +77,10 @@ def call() {
                     }
                 }
                 stage('Prepare Artifact') {     // Runs only when you run this job from tag and from branches it should run
-                    when { expression {env.TAG_NAME != null} }
+                    when { 
+                        expression {env.TAG_NAME != null} 
+                        expression {env.UPLOAD_STATUS == null} 
+                        }
                     steps {
                        sh "echo Prparing Artifacts in progress"
                        sh "npm install"
