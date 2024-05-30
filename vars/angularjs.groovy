e23c0f8f1c98775f25a53bad3104fa78ed7453b6
Hidayath <hidayath@aol.com>
2024-05-30 09:30:31 +0530

    Jenkin Pipeline

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -36,10 +36,58 @@ def call() {
                 sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate >gates.sh"
                 sh "bash gates.sh admin pass ${SONAR_URL} ${COMPONENT}"                }
             }
-        stage('Unit Testing') {
-            steps {
-                echo "Testing in Progress"
-                echo "Testing in Completed"
+        stage ('Test Cases'){
+        parallel {
+                stage('Unit Testing') {
+                     steps {
+                        sh "echo Unit Testing In Progress"
+                        // sh "pip test"
+                        sh "echo Unit Testing Completed"
+                    }
+                }
+                stage('Integration Testing') {
+                    steps {
+                       sh "echo Integration Testing In Progres"
+                    //    sh "pip verify"
+                       sh "echo Integration Testing Completed"
+                    }
+                }
+                stage('Functional Testing') {
+                    steps {
+                       sh "echo Functional Testing in progress"
+                    //    sh "pip function xxx"
+                       sh "echo Functional Testing completed"
+                    }
+                }
+                stage('Checking Arifacts availibility on NEXUS repo') {    
+                    when { expression { env.TAG_NAME != null } }
+                    steps {
+                        script {
+                          env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true") 
+                               
+                        }
+                    }
+                }
+                stage('Prepare Artifact') {  // Runs only when you run this job from tag and from branches it should run
+                    when { 
+                        expression {env.TAG_NAME != null}
+                        expression {env.UPLOAD_STATUS == ""} 
+                        }
+                    steps {
+                       sh "echo Prparing Artifacts in progress"
+                       sh "echo Artifact build  completed"
+                    }
+                }
+                stage('Uploding the Artifacts') {       // Runs only when you run this job from tag and from branches it should run
+                    when { 
+                        expression {env.TAG_NAME != null}
+                        expression {env.UPLOAD_STATUS == ""} 
+                        }
+                    steps {
+                       sh "echo Uploading the Artifacts in progress"
+                       sh "echo Artifacts Uload completed"
+                    }
+                }
             }
         }
         
--- vars/python.groovy
+++ vars/python.groovy
@@ -60,15 +60,30 @@ pipeline {
                        sh "echo Functional Testing completed"
                     }
                 }
+                stage('Checking Arifacts availibility on NEXUS repo') {    
+                    when { expression { env.TAG_NAME != null } }
+                    steps {
+                        script {
+                          env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true") 
+                               
+                        }
+                    }
+                }
                 stage('Prepare Artifact') {  // Runs only when you run this job from tag and from branches it should run
-                    when { expression {env.TAG_NAME != null} }
+                    when { 
+                        expression {env.TAG_NAME != null}
+                        expression {env.UPLOAD_STATUS == ""}
+                        }
                     steps {
                        sh "echo Prparing Artifacts in progress"
                        sh "echo Artifact build  completed"
                     }
                 }
                 stage('Uploding the Artifacts') {       // Runs only when you run this job from tag and from branches it should run
-                    when { expression {env.TAG_NAME != null} }
+                    when { 
+                        expression {env.TAG_NAME != null}
+                        expression {env.UPLOAD_STATUS == ""}
+                        }
                     steps {
                        sh "echo Uploading the Artifacts in progress"
                        sh "echo Artifacts Uload completed"
