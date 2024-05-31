f2f030c7215c50bcbb2ad94bf77597556b555320
Hidayath <hidayath@aol.com>
2024-05-31 10:19:17 +0530

    Optimizing Jenkin Pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -95,7 +95,7 @@ def artifacts() {
         stage('Uploading the artifacts') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS_CRED', passwordVariable: 'NEXUS_PASSWORD', usernameVariable: 'NEXUS_USER')]) {
             sh "echo Uploading the ${COMPONENT} Artifacts to Nexus"
-            sh "curl -f -v -u ${NEXUS_USER}:${NEXUS_PASSWORD} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+            sh "curl -f -v -u ${NEXUS_USER}:${NEXUS_PASSWORD} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.45.41:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
             echo "Artifact build completed"
         }
     }
