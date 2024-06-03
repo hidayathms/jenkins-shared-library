53c22ab90aee2e1b5c5ab79b4b89c16403e128ac
Hidayath <hidayath@aol.com>
2024-06-03 12:24:18 +0530

    Optimizing Jenkin Pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -83,9 +83,7 @@ def artifacts() {
                 sh "echo Artifact build completed"
             }
             else if(env.APP_TYPE == "angularjs") {
-                sh "ls -ltr"
-                sh "cd static/"
-                sh "zip -r ../${COMPONENT}-${TAG_NAME}.zip *" 
+            sh "zip -r .static/${COMPONENT}-${TAG_NAME}.zip *" 
                 sh "ls -ltr"
             }
             else {
