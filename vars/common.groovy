e9fc95b243e981e3ca8746f8959fd43720c12754
Hidayath <hidayath@aol.com>
2024-06-03 09:23:08 +0530

    Optimizing Jenkin Pipeline

--- vars/common.groovy
+++ vars/common.groovy
@@ -67,7 +67,7 @@ def artifacts() {
     if(env.UPLOAD_STATUS == "") {
         stage('Generating Artifacts') {
             if(env.APP_TYPE == "nodejs") {
-                sh "ls -ltr"
+                sh "sudo -s"
                 sh "dnf install npm"
                 sh "ls -ltr"
                 sh "zip ${COMPONENT}-${TAG_NAME}.zip nodemodules server.js"
