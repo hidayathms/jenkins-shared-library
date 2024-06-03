9e8465fe1a91314f992a4c6b5790ae21007c731a
Hidayath <hidayath@aol.com>
2024-06-03 09:37:15 +0530

    Optimizing Jenkin Pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,8 +6,8 @@ def call() {
         common.sonarChecks()
         common.testcases()
         env.NEXUS_URL="172.31.45.41"
-        // NPM = nodjs: 'NPM_NODEJS'
-        // env.NPM_HOME = "${NPM)"
+        NPM = nodjs: 'NPM_NODEJS'
+        env.NPM_HOME = "${NPM)"
         if(env.TAG_NAME != null) {
             common.artifacts()
         }
